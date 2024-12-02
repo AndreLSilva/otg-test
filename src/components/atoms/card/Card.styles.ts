e339c4ae10d1d7ee71f32a1589c1e4d92d6ef731
André Lima e Silva <andrelimaesilvag@gmail.com>
2024-12-02 09:49:40 -0300

    feat: updated card styles

--- src/components/atoms/card/Card.styles.ts
+++ src/components/atoms/card/Card.styles.ts
@@ -1,18 +1,35 @@
-import { Box, styled } from "@mui/material";
+import { styled } from "@mui/material";
+import { CardColors } from "./Card";
 
-export const Container = styled(Box)`
-  position: relative;
+interface ContainerProps {
+  color: CardColors;
+  fullWidth: boolean;
+}
+
+interface ContentProps {
+  padding?: number | string;
+  color: CardColors;
+}
+
+export const Container = styled("div")<ContainerProps>`
+  width: ${({ fullWidth }) => (fullWidth ? "100%" : "")};
 
   border-radius: 0.375rem;
   padding-bottom: 1.375rem;
+
+  position: relative;
+
+  background: ${({ color, theme }) => theme.palette[color].main};
 `;
 
-export const Content = styled(Box)`
+export const Content = styled("div")<ContentProps>`
   height: 100%;
   overflow: hidden;
 
   border: solid 0.125rem;
   border-radius: 0.375rem;
+  border-color: ${({ color, theme }) => theme.palette[color].main};
+  padding: ${({ padding }) => (typeof padding === "number" ? `${padding}px` : padding ?? "")};
 
   color: ${({ theme }) => theme.palette.primary.main};
   background: ${({ theme }) => theme.palette.background.paper};
--- src/components/atoms/card/Card.tsx
+++ src/components/atoms/card/Card.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import { ReactNode } from "react";
 import { Container, Content } from "./Card.styles";
 
 interface CardProps {
   className?: string;
   color?: CardColors;
+  padding?: number | string;
+  fullWidth?: boolean;
   children?: ReactNode;
 }
 
@@ -12,11 +16,15 @@ export type CardColors = "primary" | "secondary" | "tertiary";
 export function Card({
   className = undefined,
   color = "primary",
+  padding = undefined,
+  fullWidth = false,
   children = undefined,
 }: CardProps) {
   return (
-    <Container className={className} sx={{ backgroundColor: `${color}.main` }}>
-      <Content sx={{ borderColor: `${color}.main` }}>{children}</Content>
+    <Container className={className} color={color} fullWidth={fullWidth}>
+      <Content color={color} padding={padding}>
+        {children}
+      </Content>
     </Container>
   );
 }
